c688a6027f6a6c80ae961fafd6e1127113034b6c
Annihil <johndoe@example.com>
2015-10-26 22:30:41 +0100

    Movie searching (debug log only)

--- app/src/main/java/videolibrary/street/quality/qualityshow/StartActivity.java
+++ app/src/main/java/videolibrary/street/quality/qualityshow/StartActivity.java
@@ -2,16 +2,18 @@ package videolibrary.street.quality.qualityshow;
 
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class StartActivity extends Activity  {
 
-     @Override
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
 
-    }
+        startActivity(new Intent(this, SearchActivity.class));
+     }
 
     public void LaunchSignInActivity(){
 
