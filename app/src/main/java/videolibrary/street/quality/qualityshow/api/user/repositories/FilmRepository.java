d630f13b66e55b9297f382ae87fe45f25b97d436
Romain de-coster <romain.decoster.rdc@gmail.com>
2015-10-30 00:02:18 +0100

    ajout des constants pour le film

--- app/src/main/java/videolibrary/street/quality/qualityshow/api/user/repositories/FilmRepository.java
+++ app/src/main/java/videolibrary/street/quality/qualityshow/api/user/repositories/FilmRepository.java
@@ -3,6 +3,7 @@ package videolibrary.street.quality.qualityshow.api.user.repositories;
 import com.strongloop.android.loopback.ModelRepository;
 
 import videolibrary.street.quality.qualityshow.api.user.dao.Film;
+import videolibrary.street.quality.qualityshow.api.user.helpers.ApiConstants;
 
 /**
  * Created by elerion on 10/26/15.
@@ -10,6 +11,6 @@ import videolibrary.street.quality.qualityshow.api.user.dao.Film;
 public class FilmRepository extends ModelRepository<Film> {
 
     public FilmRepository() {
-        super("film", "Films", Film.class);
+        super(ApiConstants.FILM_MODEL_NAME, ApiConstants.FILM_API_NAME, Film.class);
     }
 }
