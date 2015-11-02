1a805606bd338b4f9ca449e0161adc751ec85c52
Romain de-coster <romain.decoster.rdc@gmail.com>
2015-11-02 23:56:44 +0100

    surcharge du get(Key)

--- app/src/main/java/videolibrary/street/quality/qualityshow/api/user/dao/Poster.java
+++ app/src/main/java/videolibrary/street/quality/qualityshow/api/user/dao/Poster.java
@@ -9,4 +9,18 @@ import java.util.HashMap;
  */
 public class Poster extends HashMap<String, String>{
 
+    @Override
+    public String get(Object key){
+        String tmpKey = (String) key;
+        switch (tmpKey){
+            case "thumb":
+                return this.values().toArray()[0].toString();
+            case "medium":
+                return this.values().toArray()[1].toString();
+            case "full":
+                return this.values().toArray()[2].toString();
+            default:
+                return null;
+        }
+    }
 }
