87da68a3cab85d7ea80697cf7cd78b000429bc8d
Romain decoster <romain.decoster.rdc@gmail.com>
2015-11-06 16:41:22 +0100

    Ajout de la serie en bdd

--- app/src/main/java/videolibrary/street/quality/qualityshow/activities/ShowActivity.java
+++ app/src/main/java/videolibrary/street/quality/qualityshow/activities/ShowActivity.java
@@ -4,41 +4,29 @@ import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import com.mikepenz.materialdrawer.AccountHeader;
-import com.mikepenz.materialdrawer.AccountHeaderBuilder;
-import com.mikepenz.materialdrawer.Drawer;
-import com.mikepenz.materialdrawer.DrawerBuilder;
-import com.mikepenz.materialdrawer.model.DividerDrawerItem;
-import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
-import com.mikepenz.materialdrawer.model.ProfileDrawerItem;
-import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
-import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
 import com.squareup.picasso.Picasso;
 
+import java.security.PrivateKey;
 import java.util.ArrayList;
 
 import videolibrary.street.quality.qualityshow.QualityShowApplication;
 import videolibrary.street.quality.qualityshow.R;
-import videolibrary.street.quality.qualityshow.api.user.dao.Category;
 import videolibrary.street.quality.qualityshow.api.user.dao.Film;
-import videolibrary.street.quality.qualityshow.api.user.dao.Saison;
 import videolibrary.street.quality.qualityshow.api.user.dao.Serie;
 import videolibrary.street.quality.qualityshow.api.user.dao.User;
-import videolibrary.street.quality.qualityshow.api.user.helpers.FilmHelper;
-import videolibrary.street.quality.qualityshow.api.user.helpers.SaisonHelper;
-import videolibrary.street.quality.qualityshow.api.user.helpers.SerieHelper;
-import videolibrary.street.quality.qualityshow.api.user.listeners.CategoryListener;
+import videolibrary.street.quality.qualityshow.api.user.helpers.UserHelper;
 import videolibrary.street.quality.qualityshow.api.user.listeners.FilmListener;
-import videolibrary.street.quality.qualityshow.api.user.listeners.SaisonListener;
 import videolibrary.street.quality.qualityshow.api.user.listeners.SerieListener;
 import videolibrary.street.quality.qualityshow.fragments.ProfilFragment;
 import videolibrary.street.quality.qualityshow.fragments.RecommandationsFragment;
@@ -49,7 +37,7 @@ import videolibrary.street.quality.qualityshow.listeners.ClickListener;
 /**
  * Created by Sacael on 04/11/2015.
  */
-public class ShowActivity extends AppCompatActivity implements FilmListener,SerieListener,ClickListener {
+public class ShowActivity extends AppCompatActivity implements FilmListener,SerieListener,ClickListener, View.OnClickListener {
     private Toolbar toolbar;
     private User user;
     private Serie serie;
@@ -59,26 +47,27 @@ public class ShowActivity extends AppCompatActivity implements FilmListener,Seri
     private RecommandationsFragment recommandationsFragment;
     private SettingsFragment settingsFragment;
     private ShowFragment showFragment;
+    private FloatingActionButton actionButtonActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show);
-        Intent intent=getIntent();
-        IsMovie = intent.getBooleanExtra("isMovie",true);
+        Intent intent = getIntent();
+        IsMovie = intent.getBooleanExtra("isMovie", true);
         toolbar = (Toolbar) findViewById(R.id.show_toolbar);
+        actionButtonActivity = (FloatingActionButton) findViewById(R.id.add_watch_activity);
+        actionButtonActivity.setOnClickListener(this);
         setSupportActionBar(toolbar);
-        ImageView imagev=(ImageView)findViewById(R.id.show_image);
+        ImageView imagev = (ImageView) findViewById(R.id.show_image);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         Object p;
-        if(IsMovie)
-        {
-            film=(Film)intent.getParcelableExtra("show");
+        if (IsMovie) {
+            film = (Film) intent.getParcelableExtra("show");
             getSupportActionBar().setTitle(film.getTitle());
             p = film.getPoster().get("thumb");
-        }
-        else{
-            serie = (Serie)intent.getParcelableExtra("show");
+        } else {
+            serie = (Serie) intent.getParcelableExtra("show");
             p = serie.getPoster().get("thumb");
             getSupportActionBar().setTitle(serie.getTitle());
         }
@@ -103,20 +92,21 @@ public class ShowActivity extends AppCompatActivity implements FilmListener,Seri
         transaction.commit();
 
 
-
     }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.show_menu, menu);
 
         return true;
     }
+
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
 
         switch (id) {
             case R.id.add_watch:
-                Toast.makeText(ShowActivity.this, "add", Toast.LENGTH_SHORT).show();
+                this.addSerieToUser();
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
@@ -141,7 +131,7 @@ public class ShowActivity extends AppCompatActivity implements FilmListener,Seri
 
     @Override
     public void serieIsAdded(Serie serie) {
-
+        Toast.makeText(getApplicationContext(), "Serie added", Toast.LENGTH_SHORT).show();
     }
 
     @Override
@@ -162,5 +152,16 @@ public class ShowActivity extends AppCompatActivity implements FilmListener,Seri
     @Override
     public void onItemClick(Object item) {
 
+    }
+
+    @Override
+    public void onClick(View v) {
+        this.addSerieToUser();
+    }
+
+    private void addSerieToUser(){
+        UserHelper userHelper = QualityShowApplication.getUserHelper();
+        User user = userHelper.getCurrentUser();
+        userHelper.addSerie(user, this.serie, this);
     }
 }
