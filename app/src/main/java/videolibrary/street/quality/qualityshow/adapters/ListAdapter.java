1d72a0b792e058d4215def43126944b2e9b9eaa1
GarmWyrda <edouard.samyn@gmail.com>
2015-11-08 18:40:22 +0100

    Layout and fragment profil

---
+++ app/src/main/java/videolibrary/street/quality/qualityshow/adapters/ListAdapter.java
@@ -0,0 +1,52 @@
+package videolibrary.street.quality.qualityshow.adapters;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+import videolibrary.street.quality.qualityshow.R;
+
+/**
+ * Created by EdouardEtudiant on 06/11/2015.
+ */
+public class ListAdapter extends RecyclerView.Adapter<ListAdapter.MyViewHolder> {
+
+    List<String> mListData;
+
+    public ListAdapter(List<String> mListData) {
+        this.mListData = mListData;
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.list_item,
+                viewGroup, false);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder myViewHolder, int i) {
+        myViewHolder.title.setText(mListData.get(i));
+    }
+
+    @Override
+    public int getItemCount() {
+        return mListData == null ? 0 : mListData.size();
+    }
+
+    class MyViewHolder extends RecyclerView.ViewHolder {
+
+        TextView title;
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+
+            title = (TextView) itemView.findViewById(R.id.listitem_name);
+        }
+    }
+
+}
--- app/src/main/java/videolibrary/street/quality/qualityshow/fragments/ProfilFragment.java
+++ app/src/main/java/videolibrary/street/quality/qualityshow/fragments/ProfilFragment.java
@@ -1,9 +1,112 @@
 package videolibrary.street.quality.qualityshow.fragments;
 
-import android.app.Fragment;
+import android.os.Bundle;
 
+import videolibrary.street.quality.qualityshow.R;
+
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.*;
 /**
  * Created by Pierre on 03/11/2015.
  */
 public class ProfilFragment extends Fragment {
+
+
+    public CoordinatorLayout rootView;
+    private FragmentActivity myContext;
+    private RecyclerView showsView;
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        rootView = (CoordinatorLayout) inflater.inflate(R.layout.fragment_profil, container, false);
+        showsView = (RecyclerView) rootView.findViewById(R.id.show_listView);
+
+        //final GridLayoutManager layoutManager = new GridLayoutManager(QualityShowApplication.getContext(), 3);
+        //this.showsView.setLayoutManager(layoutManager);
+
+        setupToolbar();
+        setupViewPager();
+        setupCollapsingToolbar();
+
+        return rootView;
+    }
+
+
+    private void setupCollapsingToolbar() {
+        final CollapsingToolbarLayout collapsingToolbar = (CollapsingToolbarLayout) rootView.findViewById(
+                R.id.collapse_toolbar);
+
+        collapsingToolbar.setTitleEnabled(false);
+    }
+
+    private void setupViewPager() {
+        final ViewPager viewPager = (ViewPager) rootView.findViewById(R.id.viewpager);
+        setupViewPager(viewPager);
+
+        TabLayout tabLayout = (TabLayout) rootView.findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+    private void setupToolbar() {
+        Toolbar toolbar = (Toolbar) rootView.findViewById(R.id.toolbar);
+        ((AppCompatActivity)getActivity()).setSupportActionBar(toolbar);
+        ((AppCompatActivity)getActivity()).getSupportActionBar().setTitle("TabbedCoordinatorLayout");
+        ((AppCompatActivity)getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(myContext.getSupportFragmentManager());
+        adapter.addFrag(new TabFragment(), "Tab 1");
+        adapter.addFrag(new TabFragment(), "Tab 2");
+        adapter.addFrag(new TabFragment(), "Tab 3");
+
+        viewPager.setAdapter(adapter);
+    }
+
+    static class ViewPagerAdapter extends FragmentPagerAdapter {
+
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public ViewPagerAdapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public android.support.v4.app.Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFrag(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
+    }
 }
