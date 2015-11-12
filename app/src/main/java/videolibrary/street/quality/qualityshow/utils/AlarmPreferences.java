89d22d18301b53b6d2bbf0d045fb650a64847c45
Pete <pierre.defache@gmail.com>
2015-11-12 11:50:13 +0100

    Implémentation du service des notifications

--- app/src/main/java/videolibrary/street/quality/qualityshow/utils/AlarmPreferences.java
+++ app/src/main/java/videolibrary/street/quality/qualityshow/utils/AlarmPreferences.java
@@ -1,6 +1,5 @@
 package videolibrary.street.quality.qualityshow.utils;
 
-import android.app.Activity;
 import android.content.Context;
 import android.content.SharedPreferences;
 
@@ -15,19 +14,17 @@ import videolibrary.street.quality.qualityshow.R;
  * Created by Pierre on 12/11/2015.
  */
 public class AlarmPreferences {
-    Activity activity;
 
-    public AlarmPreferences(Activity activity){
-        this.activity = activity;
+    public AlarmPreferences() {
     }
 
-    public void setAlarmPreferences(Integer newEpisodeID){
-        SharedPreferences prefs = QualityShowApplication.getContext().getSharedPreferences(activity.getString(R.string.alarm_prefs), Context.MODE_PRIVATE);
+    public void setAlarmPreferences(Integer newEpisodeID) {
+        SharedPreferences prefs = QualityShowApplication.getContext().getSharedPreferences(QualityShowApplication.getContext().getString(R.string.alarm_prefs), Context.MODE_PRIVATE);
         ArrayList<Integer> searches;
 
         Gson gson = new Gson();
-        String json = prefs.getString(activity.getString(R.string.alarm_prefs_queue), "");
-        if(json == ""){
+        String json = prefs.getString(QualityShowApplication.getContext().getString(R.string.alarm_prefs_queue), "");
+        if (json == "") {
             searches = new ArrayList<Integer>();
             searches.add(newEpisodeID);
         } else {
@@ -36,34 +33,34 @@ public class AlarmPreferences {
         }
 
         String searchesJson = gson.toJson(searches);
-        prefs.edit().putString(activity.getString(R.string.alarm_prefs_queue), searchesJson).apply();
+        prefs.edit().putString(QualityShowApplication.getContext().getString(R.string.alarm_prefs_queue), searchesJson).apply();
     }
 
-    public ArrayList<Integer> getAlarmPreferences(){
-        SharedPreferences prefs = QualityShowApplication.getContext().getSharedPreferences(activity.getString(R.string.alarm_prefs), Context.MODE_PRIVATE);
+    public ArrayList<Integer> getAlarmPreferences() {
+        SharedPreferences prefs = QualityShowApplication.getContext().getSharedPreferences(QualityShowApplication.getContext().getString(R.string.alarm_prefs), Context.MODE_PRIVATE);
         Gson gson = new Gson();
-        String json = prefs.getString(activity.getString(R.string.alarm_prefs_queue), "");
+        String json = prefs.getString(QualityShowApplication.getContext().getString(R.string.alarm_prefs_queue), "");
         ArrayList<Integer> obj = gson.fromJson(json, ArrayList.class);
 
         return obj;
     }
 
-    public boolean isInAlarmPreferences(int id){
+    public boolean isInAlarmPreferences(int id) {
         ArrayList<Integer> prefs = getAlarmPreferences();
 
-        if(prefs == null || prefs.isEmpty()){
+        if (prefs == null || prefs.isEmpty()) {
             return false;
         }
 
-        if(prefs.contains((double) id)){
+        if (prefs.contains((double) id)) {
             return true;
         }
 
         return false;
     }
 
-    public void deleteAlarmPreferences(){
-        SharedPreferences prefs = QualityShowApplication.getContext().getSharedPreferences(activity.getString(R.string.alarm_prefs), Context.MODE_PRIVATE);
-        prefs.edit().remove(activity.getString(R.string.alarm_prefs_queue)).apply();
+    public void deleteAlarmPreferences() {
+        SharedPreferences prefs = QualityShowApplication.getContext().getSharedPreferences(QualityShowApplication.getContext().getString(R.string.alarm_prefs), Context.MODE_PRIVATE);
+        prefs.edit().remove(QualityShowApplication.getContext().getString(R.string.alarm_prefs_queue)).apply();
     }
 }
